38e7f9adab6ca7637bd160193ae450b9e543dc4e
Bernardo Montoya <ber.montoya12@gmail.com>
2021-08-31 21:55:50 -0600

    fix: replace single quotes with &quot

--- components/CreateCustomRing/Landing/Landing.tsx
+++ components/CreateCustomRing/Landing/Landing.tsx
@@ -3,11 +3,11 @@ const Landing = () => {
     <div className="w-full m-auto px-4 lg:px-52 py-40 lg:py-44">
       <div className="lg:w-3/5 max-w-2xl">
         <h2 className="mb-6 lg:mb-12 text-4xl lg:text-5xl font-semibold">
-          Let's create your custom ring
+          Let&quot;s create your custom ring
         </h2>
         <p className="mb-6 lg:mb-14 text-gray-500 leading-relaxed">
-          Before we begin, we'd like to learn a bit about what you're looking
-          for so we can give you the best experience possible.
+          Before we begin, we&quot;d like to learn a bit about what you&quot;re
+          looking for so we can give you the best experience possible.
         </p>
         <div className="flex flex-wrap">
           <a
