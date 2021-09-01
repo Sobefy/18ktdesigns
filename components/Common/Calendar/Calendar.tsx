9005c77f84299b15601f08eebeb3b474302c1240
Bernardo Montoya <ber.montoya12@gmail.com>
2021-08-31 21:48:19 -0600

    fix: fix calendar types

--- components/Common/Calendar/Calendar.tsx
+++ components/Common/Calendar/Calendar.tsx
@@ -1,14 +1,15 @@
 // TODO: calculate date dynamically
-import React, { useState } from "react";
+import { useState } from "react";
 import DatePicker from "react-datepicker";
+
 import "react-datepicker/dist/react-datepicker.css";
 
 const Calendar = () => {
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   return (
     <DatePicker
       selected={selectedDate}
-      onChange={(date) => setSelectedDate(date)}
+      onChange={(date: Date) => (date ? setSelectedDate(date) : {})}
       dateFormat="dd/MM/yyyy"
       minDate={new Date()}
       maxDate={new Date(2025, 11, 31)}
