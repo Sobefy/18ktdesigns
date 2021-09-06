853323a7d0c1f8c4bf6fbb2c404157eb2ca4a5f8
Oscar Cuellar <oscar.corellan@gmail.com>
2021-09-06 09:46:14 -0600

    fix: change corrections

--- components/Common/PrimaryButton/PrimaryButton.tsx
+++ components/Common/PrimaryButton/PrimaryButton.tsx
@@ -13,7 +13,7 @@ const PrimaryButton = ({
       className={
         alternateButton
           ? "z-10 relative inline-block w-full md:w-auto py-4 px-8 mr-4 text-sm font-medium leading-normal border border-gray-300 hover:border-gray-400 rounded transision duration-200"
-          : "z-10 relativ inline-block w-full md:w-auto mb-2 py-4 px-8 mr-4 text-sm text-white font-medium leading-normal bg-red-400 hover:bg-red-300 rounded transition duration-200"
+          : "z-10 relative inline-block w-full md:w-auto mb-2 py-4 px-8 mr-4 text-sm text-white font-medium leading-normal bg-red-400 hover:bg-red-300 rounded transition duration-200"
       }
       onClick={onClick}
     >
