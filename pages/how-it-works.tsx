90776c7698aacad8858e1e0eab53f89c2f8003f8
Oscar Cuellar <oscar.corellan@gmail.com>
2021-09-15 10:19:48 -0600

    fix: change minor details on Budgeting component

--- pages/how-it-works.tsx
+++ pages/how-it-works.tsx
@@ -1,21 +1,15 @@
 import RotatingRing from "@components/HowItWorks/RotatingRing";
 import GridSection from "@components/HowItWorks/GridSection";
-<<<<<<< HEAD
 import Budgeting from "@components/HowItWorks/Budgeting/Budgeting";
-=======
 import ImageGrid from "@components/HowItWorks/ImageGrid";
->>>>>>> 85741564159f1c83c0a7871fbbaed339db6a71d3
 
 const HowItWorks = () => {
   return (
     <div className="relative py-20">
       <RotatingRing />
       <GridSection />
-<<<<<<< HEAD
       <Budgeting />
-=======
       <ImageGrid />
->>>>>>> 85741564159f1c83c0a7871fbbaed339db6a71d3
     </div>
   );
 };
