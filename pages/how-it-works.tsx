a87f6f3ca159711682bed7dccc8327bbb3e441d0
oscarcorellan <oscar.corellan@gmail.com>
2021-09-15 18:57:43 -0600

    Fix: Add PrimaryButton component into CTA Component

--- pages/how-it-works.tsx
+++ pages/how-it-works.tsx
@@ -16,7 +16,6 @@ const HowItWorks = () => {
       <CTA
         textTitleSize="md"
         titleText="Ready to create a one-of-a-kind ring with us?"
-        subtitleColor="gray"
         primarySubtitle="Drop us a line, and we'll get in touch."
         secondarySubtitle="We'll see if we're a match and how we can help each other."
         btnText="Sign up"
