610a2489d9bc43650c8092b76c867a85c8f37ce1
oscarcorellan <oscar.corellan@gmail.com>
2021-09-27 16:29:18 -0600

    Delete ReviewsComponent and move the contents into reviews , rename reviews-page as a reviews

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -1,7 +1,8 @@
 import ComposedTitles from "@components/Common/ComposedTitles";
 import PrimaryDescription from "@components/Common/PrimaryDescription";
 import ReviewsCTA from "@components/Common/ReviewsCTA";
-import ReviewsCards from "./ReviewsCards";
+import { reviewsCards } from "@lib/consts";
+import Image from "next/image";
 /* eslint-disable @next/next/no-img-element */
 const Reviews = () => {
   return (
@@ -34,7 +35,30 @@ const Reviews = () => {
             pellentesque."
             />
           </div>
-          <ReviewsCards />
+          <div className="flex flex-wrap items-center justify-around -m-3">
+            {reviewsCards.map((item) => (
+              <div className="w-full p-3 md:w-1/2 lg:w-1/3" key={item.id}>
+                <div className="p-6 border rounded-xl">
+                  <div className="mb-10">
+                    <PrimaryDescription textSize="blue" text={item.content} />
+                  </div>
+                  <div className="mb-6">
+                    <Image
+                      className="object-cover object-top w-12 h-12 rounded-full"
+                      src={item.authorImage}
+                      alt={item.nameAuthor}
+                      width={`${item.imageWidth}${item.imageUnit}`}
+                      height={`${item.imageHeight}${item.imageUnit}`}
+                    />
+                  </div>
+                  <div className="mb-2">
+                    <ComposedTitles subtitleText={item.nameAuthor} />
+                  </div>
+                  <PrimaryDescription text={item.job} />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <ReviewsCTA />
