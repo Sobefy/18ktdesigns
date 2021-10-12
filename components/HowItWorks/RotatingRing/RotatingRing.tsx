87622755f020c888ba4c8ac74a3f1025f46b799c
Oscar Cuellar <oscar.corellan@gmail.com>
2021-10-12 09:38:17 -0600

    feat: connect RotatingRing section to contentful

--- components/HowItWorks/RotatingRing/RotatingRing.tsx
+++ components/HowItWorks/RotatingRing/RotatingRing.tsx
@@ -10,40 +10,34 @@ interface RotatingRingProps {
 }
 
 const RotatingRing = ({ data }: RotatingRingProps) => {
+  const { description, heading, topHeading, mediaHolder, button } = data;
+  console.log(data);
+
   return (
     <div className="container relative px-4 pt-20 mx-auto">
       <div className="flex flex-wrap items-center -mx-4">
         <div className="w-full px-4 mb-12 lg:w-1/2 lg:mb-0">
           <div className="max-w-lg">
-            <ComposedTitles titleText="What's new at Shuffle" titleSize="sm" />
+            <ComposedTitles titleText={topHeading} titleSize="sm" />
             <div className="mt-8 mb-6 lg:mb-10 lg:pr-8">
-              <ComposedTitles
-                subtitleText="Looking for the perfect ring? Let us design it and build it
-                  for you"
-                subtitleSize="medium"
-              />
+              <ComposedTitles subtitleText={heading} subtitleSize="medium" />
             </div>
             <div className="mb-6 lg:mb-12">
-              <PrimaryDescription
-                text="Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Dolorem similique commodi dicta, officia aut, maiores animi,
-                  qui vero a at aspernatur! Nihil atque aperiam ullam!"
-                textSize="gray"
-              />
+              <PrimaryDescription text={description} textSize="gray" />
             </div>
             <a
               className="text-xl font-medium text-red-500 underline hover:no-underline"
               href="#"
             >
-              Learn more
+              {button.text}
             </a>
           </div>
         </div>
         <div className="relative w-full px-4 lg:w-1/2">
           <div className="max-w-lg">
             <ReactPlayer
-              url="https://player.vimeo.com/video/542471857?title=0&portrait=0&byline=0&autoplay=1"
-              width="100%"
+              url={mediaHolder.url}
+              width={mediaHolder.width}
               playing
               loop
             />
